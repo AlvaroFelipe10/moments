f7c6c8b6e8aabda326a5561448efb8ac3b8a388e
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-09-13 09:12:02 -0300

    resgatando dados do banco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './components/pages/about/about.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessagesComponent } from './components/messages/messages.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 
@@ -19,10 +22,11 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
     HomeComponent,
     NewMomentComponent,
+    MessagesComponent,
     MomentFormComponent,
+    AboutComponent
 
   ],
   imports: [
@@ -30,9 +34,10 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,33 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+
+import { Router } from '@angular/router';
+
+
 import { Moment } from 'src/app/Moment';
+import { MessagesService } from 'src/app/services/messages.service';
 import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
-export class NewMomentComponent implements OnInit{
+export class NewMomentComponent implements OnInit {
+  btnText: string = 'Compartilhar!';
+  image?: File;
 
-  btnText = 'Compartilhar!';
+  constructor(
+    private momentService: MomentService,
+    private router: Router,
+    private messagesService: MessagesService
+  ) {}
 
-  constructor(private momentService: MomentService){
+  ngOnInit(): void {}
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.image = file;
   }
 
-  ngOnInit(): void {
-  }
+  buildForm() {}
 
-  async createHandler(moment: Moment){
-    const formData = new FormData()
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
 
-    formData.append("title", moment.title)
-    formData.append("description", moment.description);
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
 
-    if(moment.image){
+    if (moment.image) {
       formData.append('image', moment.image);
     }
 
     await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add(`Momento adicionado com sucesso!`);
+
+    this.router.navigate(['/']);
   }
 }
