800b4a133cb945a8012482da8b3beaa182d38d18
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-09-14 14:27:38 -0300

    pagina moments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessagesComponent } from './components/messages/messages.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MomentsComponent } from './components/pages/moments/moments.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     NewMomentComponent,
     MessagesComponent,
     MomentFormComponent,
-    AboutComponent
+    AboutComponent,
+    MomentsComponent
 
   ],
   imports: [
